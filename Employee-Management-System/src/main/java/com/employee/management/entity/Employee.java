ba7d7b6db01c302a0ca39980ed00d9286d41de4d
Rasmita Das <120312766+Rasmita-Das1@users.noreply.github.com>
2023-07-06 23:25:45 +0530

    Update Employee.java
    used @Data annotation instead of manual methods and constructor!!

--- Employee-Management-System/src/main/java/com/employee/management/entity/Employee.java
+++ Employee-Management-System/src/main/java/com/employee/management/entity/Employee.java
@@ -1,9 +1,11 @@
 package com.employee.management.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "employees")
+@Data
 public class Employee {
 
 
@@ -22,50 +24,6 @@ public class Employee {
 
     @Column(name = "email")
     private String email;
-
-
-    public Employee() {
-    	
-    	
-    }
-
-    public Employee(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-    }
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
 
 }
 
